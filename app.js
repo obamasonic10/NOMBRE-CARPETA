70147c814da6ced88e53ee4b4430369b76021171
obamasonic10 <81369864+obamasonic10@users.noreply.github.com>
2022-05-07 20:13:17 -0400

    agregando carpeta

---
+++ app.js
@@ -0,0 +1,42 @@
+const { request } = require('express');
+const { response } = require('express');
+const express = require('express');
+const app =express();
+const mysql =require('mysql');
+
+require("dotenv").config();
+
+
+app.use(express.json());
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+
+connection.connect((err)=>{
+    if (err) throw err;
+    console.log('Connected to database');
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("hello world");
+});
+
+app.listen(3000,()=>{
+    console.log('servidor en puerto 3000...')
+});
+
+
+//Rutas
+app.get('/', (request, response) => {
+    response.sendFile('index.html')
+})
+
+app.get('/index', (request, response) => {
+    response.send({
+        nombre: 'EL BICHO'
+    })
+})
