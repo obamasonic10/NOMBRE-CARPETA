f88709be52d818fefd243205fa8850e4033b8ed7
obamasonic10 <81369864+obamasonic10@users.noreply.github.com>
2022-05-10 19:32:24 -0400

    agregoCrearSoli

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,4 +10,19 @@ router.get('/about', (req, res) => {
     res.render('about.html', { title: 'NOSOTROS'});
 });
 
+router.get('/index', (req, res) => {
+    res.render('index.html', { title: 'FERRETERIAS'});
+});
+
+router.get('/about.html', (req, res) => {
+    res.render('about.html', { title: 'NOSOTROS'});
+});
+
+router.get('/index.html', (req, res) => {
+    res.render('index.html', { title: 'FERRETERIAS'});
+});
+
+router.get('/crearSolicitud.html', (req, res) => {
+    res.render('crearSolicitud.html', { title: 'CREAR SOLICITUD'});
+});
 module.exports = router;
