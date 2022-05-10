dbd194364f29e46305e84fdbf2a50d5a2a6a8a1c
obamasonic10 <81369864+obamasonic10@users.noreply.github.com>
2022-05-10 19:48:10 -0400

    productos-y-ferreterias

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,4 +25,20 @@ router.get('/index.html', (req, res) => {
 router.get('/crearSolicitud.html', (req, res) => {
     res.render('crearSolicitud.html', { title: 'CREAR SOLICITUD'});
 });
+
+router.get('/productosFerreteriaDondeJuan.html', (req, res) => {
+    res.render('productosFerreteriaDondeJuan.html', { title: 'DONDE JUAN'});
+});
+
+router.get('/productosFerreteriaElMaestro.html', (req, res) => {
+    res.render('productosFerreteriaElMaestro.html', { title: 'EL MAESTRO'});
+});
+
+router.get('/productosFerreteriaMCferreteria.html', (req, res) => {
+    res.render('productosFerreteriaMCferreteria.html', { title: 'MC FERRETERIA'});
+});
+
+router.get('/productosFerreteriaOliver.html', (req, res) => {
+    res.render('productosFerreteriaOliver.html', { title: 'MC FERRETERIA'});
+});
 module.exports = router;
