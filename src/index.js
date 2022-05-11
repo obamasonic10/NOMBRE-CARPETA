e7837736ee0e82e13db6603a485a64982be5ad82
obamasonic10 <81369864+obamasonic10@users.noreply.github.com>
2022-05-11 19:48:11 -0400

    indexConecta

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+const { request } = require('express');
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql =require('mysql');
+
+require("dotenv").config();
 
 //SETTINGS
+app.use(express.json());
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
 app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
+
+connection.connect((err)=>{
+    if (err) throw err;
+    console.log('Connected to database');
+});
 
 //RUTAS
 app.use(require('./routes/index'));
